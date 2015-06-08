c06a7924248d3d15c1e7eb5fe837bf5a798386b2
sceext <sceext@foxmail.com>
2015-06-08 20:30:26 +0800

    o/ffmpeg_tkgui/gui.py, ready to test Notebook main pages

--- o/ffmpeg_tkgui/gui.py
+++ o/ffmpeg_tkgui/gui.py
@@ -1,6 +1,6 @@
 # gui.py, part for parse_video : a fork from parseVideo. 
 # gui: o/ffmpeg_tkgui/gui: parse_video Tk GUI, main gui file. 
-# version 0.0.1.0 test201506081747
+# version 0.0.3.0 test201506082029
 # author sceext <sceext@foxmail.com> 2009EisF2015, 2015.06. 
 # copyright 2015 sceext
 #
@@ -28,24 +28,32 @@
 
 from tkinter import *
 from tkinter.ttk import *
-from tkinter.font import Font
+from tkinter import tix, font, filedialog
 
 # global vars
 
 TEXT_MAIN_FONT_SIZE = 16	# 16px
-MAIN_WIN_TITLE = 'parse_video Tk GUI 1'
+MAIN_WIN_TITLE = 'ffmpeg Tk GUI,  ffmpeg 图形界面 (parse_video) 1 '
 MAIN_FONT_NAME = '微软雅黑'
+MAIN_PAGE_NAME = [
+    '  合 并  ', 
+    '  日 志  ', 
+    '  设 置  ', 
+    '  帮 助  ', 
+]
 
 # functions
 
 # class
 
 # main window
-
 class MainWin(object):
     
     def __init__(self):
         self.root = None	# root window
+        self.nb = None		# tkinter.ttk Notebook
+        self.p = []		# Notebook pages
+        self.pf = []		# Notebook pages Frames
         
         # TODO
         
@@ -59,28 +67,48 @@ class MainWin(object):
         # set main window title
         root.title(MAIN_WIN_TITLE)
         
-        # TODO
-        # create style for ttk
+        # create fonts and set styles
+        mf = font.Font(root, size=TEXT_MAIN_FONT_SIZE)	# main font
         style = Style()
-        # set font
         style.configure('.', font=(MAIN_FONT_NAME, TEXT_MAIN_FONT_SIZE))
         style.configure('My.TEntry', padding=5)
         
-        # create font for main Text and man Entry
-        f = Font(root, size=TEXT_MAIN_FONT_SIZE)
+        # create Notebook and each pages
+        nb = Notebook(root, padding=0)
+        self.nb = nb
+        # create each page with name
+        for i in range(len(MAIN_PAGE_NAME)):
+            pf = Frame(nb)
+            nb.add(pf, text=MAIN_PAGE_NAME[i])
+            self.pf.append(pf)
+        # create sub part
+        pf = self.pf
+        # create merge
+        p = PartMerge()
+        p.start(pf[0])
+        self.p.append(p)
+        # create log
+        p = PartLog()
+        p.start(pf[1], font=mf)
+        self.p.append(p)
+        # create Setting
+        p = PartSetting()
+        p.start(pf[2])
+        self.p.append(p)
+        # create about part
+        p = PartAbout()
+        p.start(pf[3], font=mf)
+        self.p.append(p)
         
-        # create frame
-        f0 = Frame(root)	# top part, url text Entry and main Button
-        f1 = Frame(root)	# bottom part, main Text and scrollbars
-        f0.pack(side=TOP, fill=X, expand=False)
-        f1.pack(side=BOTTOM, fill=BOTH, expand=True)
-        
-        # create textvar for main Entry, and hd Entry
-        v1 = StringVar()
-        self.text_entry_var = v1
-        v2 = StringVar()
-        self.hd_entry_var = v2
-        # create main window done
+        # pack main Notebook
+        nb.pack(side=TOP, fill=BOTH, expand=True)
+        # TODO
+        # create UI done
+    
+    # show one notebook page
+    def show_page(self, p_n):
+        # TODO
+        pass
     
     # start main loop
     def mainloop(self):
@@ -88,6 +116,162 @@ class MainWin(object):
         mainloop()
     
     # end MainWin class
+
+# entry box, easy text Entry support
+class EntryBox(object):
+    
+    def __init__(self):
+        self.parent = None
+        self.e = None	# Entry
+        self.v = None	# StringVar
+    
+    # create sub elements
+    def start(self, parent, font=None, style='TEntry'):
+        # create UI
+        v = StringVar()
+        e = Entry(parent, textvariable=v, font=font, style=style)
+        # NOTE should be pack
+        # save it
+        self.parent = parent
+        self.v = v
+        self.e = e
+        # create UI done
+    
+    # entry operations
+    def get_text(self):
+        return self.v.get()
+    
+    def set_text(self, text):
+        self.v.set(text)
+    
+    # end EntryBox class
+
+# text part, TextBox, Text and Scrollbars, simple text part
+class TextBox(object):
+    
+    def __init__(self):
+        self.parent = None	# parent element
+        
+        self.t = None	# Text
+        self.sx = None	# x Scrollbar
+        self.sy = None	# y Scrollbar
+        
+        # init end
+    
+    # start create sub elements
+    def start(self, parent, font=None, bg='#ddd', fg='#333'):	# parent element
+        # create UI
+        sx = Scrollbar(parent, orient=HORIZONTAL)
+        sy = Scrollbar(parent, orient=VERTICAL)
+        t = Text(parent, wrap=NONE, font=font, padx=0, pady=0, relief=FLAT, bd=0, bg=bg, fg=fg)
+        
+        # pack it
+        sy.pack(side=RIGHT, fill=Y)
+        sx.pack(side=BOTTOM, fill=X)
+        t.pack(side=TOP, fill=BOTH, expand=True)
+        # set scrollbar
+        sx.config(command=t.xview)
+        sy.config(command=t.yview)
+        t.config(xscrollcommand=sx.set)
+        t.config(yscrollcommand=sy.set)
+        
+        # save it
+        self.parent = parent
+        self.t = t
+        self.sx = sx
+        self.sy = sy
+        # create UI done
+    
+    # text part operations
+    def enable(self):
+        self.t.config(state=NORMAL)
+    
+    def disable(self):
+        self.t.config(state=DISABLED)
+    
+    def get_text(self):	# get all text
+        return self.t.get('1.0', END)
+    
+    def clear(self):	# delete all text
+        self.t.delete('1.0', END)
+    
+    def add_text(self, text='', pos=END):
+        self.t.insert(pos, text)
+    
+    # end TextBox class
+
+# merge part
+class PartMerge(object):
+    
+    def __init__(self):
+        self.parent = None
+        pass
+    
+    def start(self, parent):
+        pass
+    
+    # end PartMerge class
+
+# log part
+class PartLog(object):
+    
+    def __init__(self):
+        self.parent = None
+        
+        self.t = None	# TextBox
+        
+    
+    def start(self, parent, font=None):
+        
+        # just create TextBox
+        self.t = TextBox()
+        self.t.start(parent, font=font)
+        # TODO
+        pass
+    
+    # end PartLog class
+
+# setting part
+class PartSetting(object):
+    
+    def __init__(self):
+        self.parent = None
+        pass
+    
+    def start(self, parent):
+        pass
+    
+    # end PartSetting class
+
+# about part
+class PartAbout(object):
+    
+    def __init__(self):
+        self.parent = None
+        
+        self.t = None	# sub TextBox
+    
+    def start(self, parent, font=None):
+        # just create TextBox
+        self.t = TextBox()
+        self.t.start(parent, font=font)
+        # TODO
+        pass
+    
+    # end PartAbout class
+
+# debug
+def test():
+    # just create and show UI
+    w = MainWin()
+    w.start()
+    # start mainloop
+    w.mainloop()
+    # test done
+
+# auto start test
+if __name__ == '__main__':
+    test()
 
 # end gui.py
 
